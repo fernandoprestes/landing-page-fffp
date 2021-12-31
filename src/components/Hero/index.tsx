c89631e03e29eabc596802a6df72bb3a5430b0fb
Fernando Prestes <fernandoprestesc@gmail.com>
2021-12-31 16:02:48 -0300

    refact: change hero to main

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -4,8 +4,8 @@ import './styles.scss'
 
 const Hero = () => {
   return (
-    <main className="container mx-auto max-w-7xl flex items-center">
-      <section className="w-full md:w-3/5 text-mint-cream p-10 ">
+    <div className="hero-banner mx-auto max-w-7xl flex items-center">
+      <section className="w-full md:w-3/5 text-mint-cream p-10">
         <p className="text-2xl md:text-4xl lg:text-5xl">
           <img
             className="w-8 md:w-10 lg:w-12 inline mb-2"
@@ -16,7 +16,8 @@ const Hero = () => {
           <span className="text-6xl lg:text-8xl font-bold mb-2 inline-block w-full">
             Fernando
           </span>
-          frontend developer<span className="text-sea-green font-bold">_</span>
+          frontend developer
+          <span className="text-sea-green font-bold">_</span>
         </p>
 
         <button className="px-4 py-2 w-52 mt-8 text-sea-green border border-sea-green">
@@ -27,7 +28,7 @@ const Hero = () => {
       <section className="hidden md:block w-3/5">
         <img src={HeroBanner} alt="banner" />
       </section>
-    </main>
+    </div>
   )
 }
 
---
+++ src/components/Main/index.tsx
@@ -0,0 +1,13 @@
+import About from '../About'
+import Hero from '../Hero'
+
+const Main = () => {
+  return (
+    <main className="container  mx-auto">
+      <Hero />
+      <About />
+    </main>
+  )
+}
+
+export default Main
