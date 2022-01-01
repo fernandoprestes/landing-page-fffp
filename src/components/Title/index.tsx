369c3511cdb02823cb4d09e756618bc1c3549f32
Fernando Prestes <fernandoprestesc@gmail.com>
2022-01-01 11:55:12 -0300

    feat: create title

---
+++ src/components/Title/index.tsx
@@ -0,0 +1,14 @@
+import TextBold from '../TextBold'
+
+interface TitleProps {
+  text: string
+}
+
+export default function Title({ text }: TitleProps) {
+  return (
+    <h2 className="text-2xl md:text-4xl lg:text-5xl mb-10 md:mb-20">
+      {text}
+      <TextBold text="_" />
+    </h2>
+  )
+}
